c34f9ac211702b75a1bcd0bb3065aba9a93e8390
Elton <eltonjf@gmail.com>
2018-04-26 01:49:44 -0300

    Configuração com BD e DateTime Java 8

--- src/main/java/com/example/springjwtreact/SpringJwtReactApplication.java
+++ src/main/java/com/example/springjwtreact/SpringJwtReactApplication.java
@@ -1,12 +1,27 @@
 package com.example.springjwtreact;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		SpringJwtReactApplication.class,
+		Jsr310JpaConverters.class })
 public class SpringJwtReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJwtReactApplication.class, args);
 	}
+	
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
 }
