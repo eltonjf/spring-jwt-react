9469d681572775db2cc7cd14fd25702f4c4eaa3f
Elton <eltonjf@gmail.com>
2018-04-26 01:35:22 -0300

    Primeiro commit com o projeto simplesmente criado

---
+++ src/main/java/com/example/springjwtreact/SpringJwtReactApplication.java
@@ -0,0 +1,12 @@
+package com.example.springjwtreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtReactApplication.class, args);
+	}
+}
